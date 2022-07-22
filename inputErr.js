2bd7cb3c3339e080b52f1052d36e9d4f1e54b60d
noman-swe <noman.swe130@gmail.com>
2022-07-23 00:28:22 +0600

    22-4 Handle unexpected function input parameter error

---
+++ inputErr.js
@@ -0,0 +1,9 @@
+function addition(num1, num2){
+    const result = num1 + num2;
+    return result;
+}
+
+const add = addition(1 - 2); //be consious about paramiter value
+const add2 = addition(1, 2);
+console.log(add);
+console.log(add2);
