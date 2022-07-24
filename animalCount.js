5a60c127dee67585bf50484ad48dad0dbc161768
noman-swe <noman.swe130@gmail.com>
2022-07-24 14:40:08 +0600

    22-9 Traveling in a Jungle and counting wild animals

---
+++ animalCount.js
@@ -0,0 +1,33 @@
+
+
+
+function animalCount(miles) {
+    const animalDensityFirst10Miles = 10;
+    const animalDensitySecond10Miles = 50;
+    const animalDensityThird10Miles = 100;
+
+    if (miles <= 10) {
+        const count = miles * animalDensityFirst10Miles;
+        return count;
+    }
+    else if (miles <= 20) {
+        const first10Miles = 10 * animalDensityFirst10Miles;
+        const restMiles = miles - 10;
+        const secondMilesAnimals = restMiles * animalDensitySecond10Miles;
+        const count = first10Miles + secondMilesAnimals;
+        return count;
+        
+    }
+    else{
+        const first10Miles = 10 * animalDensityFirst10Miles;
+        const secondMilesAnimals = 10 * animalDensitySecond10Miles;
+        const restMiles = miles - 20;
+        const thirdMilesAnimals = restMiles * animalDensityThird10Miles; 
+        const thirdCount = first10Miles + secondMilesAnimals + thirdMilesAnimals;
+        return thirdCount;
+    }
+    
+}
+
+const totalAnimal = animalCount(27);
+console.log(totalAnimal);
