7b32f302a6bf8ab2b9c9596e371eb3e6c86a6f02
noman-swe <noman.swe130@gmail.com>
2022-07-24 23:59:13 +0600

    My Problem Solving Practice

--- BasicJs-Extra/problemSolving.js
+++ BasicJs-Extra/problemSolving.js
@@ -1,1 +1,32 @@
 //  fruit Price
+
+const fruits = [
+    {name: 'Banana', price: 5, quantity: 4},
+    {name: 'Mango', price: 20, quantity: 5},
+    {name: 'Orange', price: 25, quantity: 7},
+    {name: 'Apple', price: 15, quantity: 9},
+    {name: 'Pineapple', price: 30, quantity: 12},
+]
+
+function cheapestFruit(){
+
+    let cheapest = fruits[0];
+    for(const fruit of fruits){
+        if(fruit.price < cheapest.price){
+            cheapest = fruit;
+        }
+    }
+    console.log('Cheapest Fruit:', cheapest.name);
+}
+cheapestFruit();
+
+function fruitsPrice(){
+    let tempPrice = 0;
+    for(const fruit of fruits){
+        const fruitQuantityPrice = fruit.quantity * fruit.price; 
+        tempPrice = fruitQuantityPrice + tempPrice;
+    }
+    console.log('Total Price: ',tempPrice , 'taka');
+}
+
+fruitsPrice();
